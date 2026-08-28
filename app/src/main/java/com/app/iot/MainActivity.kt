d09988a3e1878d6c3199710894ab96ece540b142
Rahul <sumitojha264@gmail.com>
2026-08-28 15:56:36 +0530

    Refine ui.

--- app/src/main/java/com/app/iot/MainActivity.kt
+++ app/src/main/java/com/app/iot/MainActivity.kt
@@ -2,41 +2,18 @@ package com.app.iot
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsTopHeight
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.toArgb
 import com.app.iot.nav.AppNavHost
-import com.app.iot.ui.theme.AppPalette
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.dark(AppPalette.secondary.toArgb())
-        )
-        setContent {
-            Box(modifier = Modifier.fillMaxSize()) {
-                AppNavHost()
-                Box(
-                    modifier = Modifier
-                        .align(Alignment.TopCenter)
-                        .fillMaxWidth()
-                        .windowInsetsTopHeight(WindowInsets.statusBars)
-                        .background(AppPalette.secondary)
-                )
-            }
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			AppNavHost()
+		}
+	}
 }
